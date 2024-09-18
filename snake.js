9cb1d2d858a04cae58b89a3400df8a934d45aba0
Ublec01d <ublec01d@gmail.com>
2024-09-18 20:42:25 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -50,7 +50,7 @@ function gameLoop() {
                 food = generateRandomPosition();
                 score++;
                 updateScore();
-                speed = Math.max(0.1, speed * 0.98);  // Slower increase (reduce by 2% each time)
+                speed = Math.max(0.1, speed * 0.95);  // Slower increase (reduce by 5% each time)
                 checkAndUpdateHighScore();  // Check if we need to update the high score
             }
             drawGame();
